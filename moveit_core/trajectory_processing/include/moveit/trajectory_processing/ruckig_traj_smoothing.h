086db2e52fd35b285bda780dbc33d4e9c05d741f
AndyZe <zelenak@picknik.ai>
2022-04-12 10:22:07 -0500

    Ruckig smoothing cleanup (#1111)
    * Simplify. Use Ruckig as a check if duration needs extension.
    
    * Add a test for a single waypoint
    
    * Add a unit test that actually requires math
    
    * Use a traj deep copy. Right-const everywhere. Minor cleanup.
    
    * Fix the test timestep
    
    * Read jerk from the robot model
    
    * Revert to right-const everywhere
    
    * Simplify jerk limit expression
    
    Co-authored-by: Henning Kayser <henningkayser@picknik.ai>
    
    * Add a helpful hint if no vel/accel/jerk limits are defined.
    
    * Update test conditions since I changed the jerk limit
    
    * Rebase cleanup
    
    Co-authored-by: Henning Kayser <henningkayser@picknik.ai>

--- moveit_core/trajectory_processing/include/moveit/trajectory_processing/ruckig_traj_smoothing.h
+++ moveit_core/trajectory_processing/include/moveit/trajectory_processing/ruckig_traj_smoothing.h
@@ -31,7 +31,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 
-/* Author: Jack Center, Wyatt Rees, Andy Zelenak */
+/* Author: Jack Center, Wyatt Rees, Andy Zelenak, Stephanie Eng */
 
 #pragma once
 
@@ -61,33 +61,6 @@ private:
   static void getNextRuckigInput(const ruckig::OutputParameter<0>& ruckig_output,
                                  const moveit::core::RobotStatePtr& next_waypoint, size_t num_dof,
                                  const std::vector<int>& idx, ruckig::InputParameter<0>& ruckig_input);
-
-  /**
-   * \brief Check for lagging motion of any joint at a waypoint.
-   * \param num_dof       Number  of actuated joints
-   * \param ruckig_input  Input parameters to Ruckig
-   * \param ruckig_output Output parameters from Ruckig
-   * \return              true if lagging motion is detected on any joint
-   */
-  static bool checkForLaggingMotion(const size_t num_dof, const ruckig::InputParameter<0>& ruckig_input,
-                                    const ruckig::OutputParameter<0>& ruckig_output);
-
-  /**
-   * \brief Return L2-norm of velocity, taking all joints into account.
-   * \param ruckig_input  Input parameters to Ruckig
-   * \param num_dof       Number  of actuated joints
-   */
-  static double getTargetVelocityMagnitude(const ruckig::InputParameter<0>& ruckig_input, size_t num_dof);
-
-  /**
-   * \brief Check if the joint positions of two waypoints are very similar.
-   * \param prev_waypoint State at waypoint i-1
-   * \param prev_waypoint State at waypoint i
-   * \joint_group         The MoveIt JointModelGroup of interest
-   */
-  static bool checkForIdenticalWaypoints(const moveit::core::RobotState& prev_waypoint,
-                                         const moveit::core::RobotState& next_waypoint,
-                                         const moveit::core::JointModelGroup* joint_group);
 
   /**
    * \brief Initialize Ruckig position/vel/accel. This initializes ruckig_input and ruckig_output to the same values
